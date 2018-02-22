2a4fcda95adb58a1321cef3ead19f882503b375e
alon.ashkenazi <alon.ashkenazi@600900B211DE408>
2018-02-22 16:03:32 +1100

    Created a Gone Home game

---
+++ Assets/~GoneHome/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI;
+// Clean code: CTRL + K + D (In that order)
+
+namespace GoneHome
+{
+    public class Enemy : MonoBehaviour
+    {
+        public Transform target;
+
+        private NavMeshAgent agent;
+
+        // Use this for initialization
+        void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+}
---
+++ Assets/~GoneHome/Scripts/Player.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GoneHome
+{
+    public class Player : MonoBehaviour
+    {
+        public float movementSpeed = 10f;
+
+        private Rigidbody rigid;
+
+        private Transform cam; // << Added this!
+
+        private float currentY;
+
+        // Use this for initialization
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody>();
+
+            cam = Camera.main.transform; //<<ADDED
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Movement();
+            Rotation();
+        }
+
+        void Rotation()
+        {
+            if(Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                currentY += 90;
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                currentY -= 90;
+            }
+
+            Quaternion rotation = Quaternion.AngleAxis(currentY, Vector3.up);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, 0.1f);
+        }
+        void Movement()
+        {
+        
+            float inputV = Input.GetAxis("Vertical");
+
+            Vector3 inputDir = transform.forward * inputV;
+
+            // Copy position
+            Vector3 position = transform.position;
+            // Offset the new position
+            position += inputDir * movementSpeed * Time.deltaTime;
+            // Apply new position to rigidbody
+            rigid.MovePosition(position);
+        }
+    }
+}
