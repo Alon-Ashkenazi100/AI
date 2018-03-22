937cfa537b52dcadabdad7eb05fce0e7d224d4a0
alon.ashkenazi <alon.ashkenazi@600900B211DE408>
2018-03-22 16:05:42 +1100

    Added new tiled levels

--- Assets/~SunnyLand/LevelSettings/SunnyLand.tsx
+++ Assets/~SunnyLand/LevelSettings/SunnyLand.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="SunnyLand" tilewidth="16" tileheight="16" tilecount="575" columns="25">
- <image source="../Source/Repos/AI/Assets/~SunnyLand/Environment/tileset.png" width="400" height="368"/>
+ <image source="../Environment/tileset.png" width="400" height="368"/>
 </tileset>
