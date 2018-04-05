101c948b9be9bf15c3c3004a9910af6804f2a9a7
Ashkenazi <Ashkenazi@GUEST>
2018-04-05 16:04:21 +1000

    Edited PlayerController Movement and Jump

--- Assets/~SunnyLand/Scripts/PlayerController.cs
+++ Assets/~SunnyLand/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ namespace SunnyLand
         public float speed = 5f; // movement speed
         public int health = 100;
         public int damage = 50;
+        [Tooltip("The force that's applied when hit by another object")]
         public float hitForce = 4f; //Force applied when players hits an object
         public float damageForce = 4f; // Force applied when player is hit by object
         public float maxVelocity = 3f; // Maximum Velocity to limit the player to
@@ -44,14 +45,32 @@ namespace SunnyLand
         // Update is called once per frame
         void Update()
         {
-
+            PerformClimb();
+            PerformMove();
+            PerformJump();
+        }
+        void FixedUpdate()
+        {
+            DetectGround();
+        }
+        void OnDrawGizmos()
+        {
+            // Draw the ground ray
+            Ray groundRay = new Ray(transform.position, Vector3.down);
+            Gizmos.DrawLine(groundRay.origin,
+                            groundRay.origin + groundRay.direction * rayDistance);
+            // Draw direction line
+            Vector3 right = Vector3.Cross(groundNormal, Vector3.forward);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.position - right * 1f,
+                            transform.position + right * 1f);
         }
         #endregion
 
         #region Custom Functions
         public void Jump()
         {
-
+            isJumping = true;
         }
         public void Crouch()
         {
@@ -63,7 +82,14 @@ namespace SunnyLand
         }
         public void Move(float horizontal)
         {
-
+            // If there is horizontal input
+            if (horizontal != 0)
+            {
+                // Flip the sprite in the correct direction
+                rend.flipX = horizontal < 0;
+            }
+            // Store the input for later
+            inputH = horizontal;
         }
         public void Climb(float vertical)
         {
@@ -74,13 +100,80 @@ namespace SunnyLand
 
         }
 
-        private void DetectGround()
+        private void PerformClimb()
         {
 
         }
-        private void CheckGround(RaycastHit2D hit)
+        private void PerformMove()
         {
+            // Calculate the move direction based on surface
+            Vector3 right = Vector3.Cross(groundNormal, Vector3.forward);
+            // Add force in direction using horizontal input
+            rigid.AddForce(right * inputH * speed);
+            // Limit the velocity to max velocity
+            LimitVelocity();
+        }
+        private void PerformJump()
+        {
+            // Are we jumping?
+            if (isJumping)
+            {
+                // Are we allowed to jump?
+                if(currentJump < maxJumpCount)
+                {
+                    //Increment jump by 1
+                    currentJump++;
+                    rigid.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+                }
+            }
+        }
 
+        private void DetectGround()
+        {
+            // Create a ground ray
+            Ray groundRay = new Ray(transform.position, Vector3.down);
+            // Shoot ray below the player and get all the hits
+            RaycastHit2D[] hits = Physics2D.RaycastAll(groundRay.origin,
+                                                       groundRay.direction,
+                                                       rayDistance);
+            // Loop through all the hits
+            foreach (var hit in hits)
+            {
+                // Check if we hit an enemy
+                CheckEnemy(hit);
+                // Check if we hit the ground
+                if (CheckGround(hit))
+                {
+                    // Exit the loop
+                    break;
+                }
+            }
+        }
+        private bool CheckGround(RaycastHit2D hit)
+        {
+            if(hit.collider != null &&         // Exists AND
+               hit.collider.name != name &&     // is not the player AND
+               hit.collider.isTrigger == false) // is not a trigger
+            {
+                // Reset the jump
+                currentJump = 0;
+                // Player is in the grounded state
+                isGrounded = true;
+                // Update the ground normal
+                groundNormal = hit.normal;
+
+                // Check for slopes
+                float slopeAngle = Vector3.Angle(Vector3.up, hit.normal);
+                isOnSlope = Mathf.Abs(slopeAngle) > 0 &&
+                            Mathf.Abs(slopeAngle) < maxSlopeAngle;
+                // If we reached the max slope
+
+                // Return true! (ground is found)
+                return true;
+            }
+
+            // Return false! (ground is not found)
+            return false;
         }
         private void CheckEnemy(RaycastHit2D hit)
         {
@@ -88,7 +181,16 @@ namespace SunnyLand
         }
         private void LimitVelocity()
         {
-
+            // Cache rigid velocity into smaller variable
+            Vector3 vel = rigid.velocity;
+            // If vel length is greater than max velocity
+            if (vel.magnitude > maxVelocity)
+            {
+                // Cap the velocity to maxVelocity
+                vel = vel.normalized * maxVelocity;
+            }
+            // Apply newly calculated vel to rigidbody
+            rigid.velocity = vel;
         }
         private void StopClimbing()
         {
